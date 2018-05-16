b20897ab9cd350839dffdba5bbb3f9fa740bab7d
Schurter.Caitlyn <Schurter.Caitlyn@SEMS-259-33.SEMS259.LAB>
2018-05-16 16:28:51 -0700

    5/16/18

---
+++ src/ForLoopgauntlet.java
@@ -0,0 +1,53 @@
+
+public class ForLoopgauntlet {
+public static void main(String[] args) {
+//	System.out.println("Test 1");
+	for(int i = 0;i <101;i++) {
+//		System.out.println(i);
+	}
+	
+//	System.out.println("Test 2");
+//	for(int i = 100;i>=0;i--) {
+//		System.out.println(i);
+//	}
+	
+//	System.out.println("Test 3");
+//	for(int i = 2;i <=100;i+=2) {
+//		System.out.println(i);
+//	}
+	
+//	System.out.println("Test 4");
+//	for(int i = 1;i <100;i+=2) {
+//		System.out.println(i);
+//	}
+
+	System.out.println("Test 5");
+	for(int i = 1;i <=500;i++) {
+		if(i%2 == 0) {
+			System.out.println(i + " is even");
+		}
+		
+		else {
+			System.out.println(i + " is odd");
+		}
+		
+		
+		
+		
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
+}
--- src/GoofyNames.java
+++ src/GoofyNames.java
@@ -9,27 +9,34 @@ public class GoofyNames {
 	public static void main(String[] args) {
 		String goofyName = "";
 
-		// 1. Ask the user to enter their name
-
-		// 2. Print upper case name to the console using .toUpperCase()
-		//    Run your program to see that this works.
-
-		// 3. Loop through each character of the name (steps 4 - 6).
-		//    HINT: Use .length() to determine the number of characters in the String.
-
+			// 1. Ask the user to enter their name
+		String name = JOptionPane.showInputDialog("What tis ur name?");
+			// 2. Print upper case name to the console using .toUpperCase()
+			//    Run your program to see that this works.
+		JOptionPane.showMessageDialog(null, name.toUpperCase());
+			// 3. Loop through each character of the name (steps 4 - 6).
+			//    HINT: Use .length() to determine the number of characters in the String.
+		for(int i = 0;i < name.length();i++) {
+		
 			// 4. Create a String variable to store the next character of the name
 			//    using .substring(start, end)
 			//    HINT: replace 'start' and 'end' to get String with character at i 
-			
-			// 5. Use MODULO to set this variable to upper case for EVEN characters
+		String character = name.substring(i,i+1);
+					// 5. Use MODULO to set this variable to upper case for EVEN characters
 			//    and lower case for ODD characters.
-
+		if(i%2==1) {
+		character = character.toLowerCase();	
+		}
+		else {
+		character = character.toUpperCase();	
+		}
 			
 			// 6. ADD this String  (containing 1 char) to the goofyName String
-
+		goofyName = goofyName + character;
 		
-		// 7. Use pop-up to show user their Goofy name
-
+		}
+			// 7. Use pop-up to show user their Goofy name
+		JOptionPane.showMessageDialog(null, goofyName);
 	}
 }
 
